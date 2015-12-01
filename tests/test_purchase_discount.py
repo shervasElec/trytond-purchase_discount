a10c8a888288c95c88ca08ef994e318c1c6ca034
resteve <resteve@zikzakmedia.com>
2015-12-01 11:17:16 +0100

    Tests

--- tests/test_purchase_discount.py
+++ tests/test_purchase_discount.py
@@ -1,28 +1,18 @@
-#!/usr/bin/env python
+# This file is part of the purchase_discount module for Tryton.
 # The COPYRIGHT file at the top level of this repository contains the full
 # copyright notices and license terms.
-from trytond.tests.test_tryton import test_view, test_depends
-import trytond.tests.test_tryton
 import unittest
+import trytond.tests.test_tryton
+from trytond.tests.test_tryton import ModuleTestCase
 
 
-class PurchaseDiscountTestCase(unittest.TestCase):
+class PurchaseDiscountTestCase(ModuleTestCase):
     'Test Purchase Discount module'
-
-    def setUp(self):
-        trytond.tests.test_tryton.install_module('purchase_discount')
-
-    def test0005views(self):
-        'Test views'
-        test_view('purchase_discount')
-
-    def test0006depends(self):
-        'Test depends'
-        test_depends()
+    module = 'purchase_discount'
 
 
 def suite():
     suite = trytond.tests.test_tryton.suite()
     suite.addTests(unittest.TestLoader().loadTestsFromTestCase(
-            PurchaseDiscountTestCase))
+        PurchaseDiscountTestCase))
     return suite
