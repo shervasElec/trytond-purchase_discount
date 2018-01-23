38705ee23278e832e3ede6d24c1e45381651bb29
C?dric Krier <ced@b2ck.com>
2018-01-23 18:34:27 +0100

    Fix style

--- __init__.py
+++ __init__.py
@@ -1,14 +1,15 @@
 # The COPYRIGHT file at the top level of this repository contains the full
 # copyright notices and license terms.
 from trytond.pool import Pool
-from .purchase import *
-from .move import *
+from . import purchase
+from . import move
+
 
 def register():
     Pool.register(
-        PurchaseLine,
-        Move,
+        purchase.PurchaseLine,
+        move.Move,
         module='purchase_discount', type_='model')
     Pool.register(
-        PurchaseDiscountReport,
+        purchase.PurchaseDiscountReport,
         module='purchase_discount', type_='report')
